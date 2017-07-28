7aaec72bdd3277fb4ab58bc2e6e9b8a26603f32a
Maxim <osadchuk.m.01@gmail.com>
2017-07-28 15:13:27 +0300

    Include Coverage.

--- manage.py
+++ manage.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 import os
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 import flask_whooshalchemy as wa
 from app import create_app, db
 from app.models import User, Post, Comment, Widget
@@ -32,6 +39,7 @@ def bootstrap():
     Post._bootstrap()
     Comment._bootstrap()
     Widget._bootstrap()
+    os.environ['ADMIN_EMAIL'] = 'osadchuk.m.01@gmail.com'
     db.session.add(User(email='osadchuk.m.01@gmail.com', name='Maxim', password='1111'))
     db.session.commit()
 
@@ -39,9 +47,23 @@ def bootstrap():
 @manager.command
 def test(coverage=False):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 
 if __name__ == '__main__':
